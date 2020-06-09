43f873923d6ae102b9ad46de806d02bc1c319d88
oleoko <oleoko88@gmail.com>
2020-06-09 20:47:11 +0300

    add comment to function; capitalize first letter of comments

--- Task_1/Task1.py
+++ Task_1/Task1.py
@@ -1,15 +1,15 @@
 from collections import defaultdict
 
-logfile = open("LogFile.txt") # open file with logs
+logfile = open("LogFile.txt") # Open file with logs
 ip_list = defaultdict(int)
 url_list = defaultdict(int)
 
-# add ip and url to dict and count them
+# Add ip and url to dict and count them
 for i in logfile:
     ip_list[i.split(" ")[0]] += 1
     url_list[i.split(" ")[8]] += 1
 
-
+# Sort dictionary function
 def print_sorted(dict):
     sorted_ = {k: v for k, v in sorted(dict.items(), reverse = True, key=lambda item: item[1])}
     for item in sorted_:
